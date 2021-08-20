ed908df631ea22912f026ede25559c4bcbe08fdb
qqgameover <kasper35@live.no>
2021-08-20 15:06:03 +0200

    Update fetch.js

--- fetch.js
+++ fetch.js
@@ -6,3 +6,17 @@ const fetchedData = fetch(url)
 	.then(response => response.json())
 	.then(data => data.rates)
 	.then(data => console.log(data))
+
+async function fetchData() {
+	let response = await fetch(url)
+	let data = await response.json();
+	dataSave = data.rates;
+	console.log(dataSave)
+}
+async function filterData() {
+	await fetchData()
+	const toArray = Object.entries(dataSave);
+	console.log(toArray)
+	const filteredData = toArray.filter(toArray => toArray[1] > 10);
+	console.log(filteredData)
+}
