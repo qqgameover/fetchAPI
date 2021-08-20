51f73fa172b41c2dc634c140fed4f35f07b6ea50
qqgameover <kasper35@live.no>
2021-08-20 13:46:23 +0200

    Update fetch.js

--- fetch.js
+++ fetch.js
@@ -2,7 +2,7 @@ const url = "http://api.coinlayer.com/api/live?" + secret;
 
 let dataSave;
 
-fetch(url)
+const fetchedData = fetch(url)
 	.then(response => response.json())
 	.then(data => data.rates)
 	.then(data => console.log(data))
